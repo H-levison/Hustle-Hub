d68a961b63658bf688f4ff01c9ef3f1e4d4ed960
Hikma Hamza <h.hamza@alustudent.com>
2025-07-21 12:11:14 +0000

    Update on VendorBanner ; Additon of appropriate text and images

--- src/components/VendorBanner.tsx
+++ src/components/VendorBanner.tsx
@@ -6,10 +6,10 @@ const VendorBanner: React.FC = () => (
       {/* Left Side */}
       <div className="flex-1 max-w-xl text-center md:text-left">
         <h2 className="text-white text-2xl sm:text-3xl md:text-4xl font-extrabold mb-3 sm:mb-4 leading-tight">
-          Join as a VENDOR: showcase<br className="hidden sm:block" />your products and services Today
+          Showcase<br className="hidden sm:block" />your products and services today!
         </h2>
         <p className="text-white/80 mb-6 sm:mb-8 text-sm sm:text-base md:text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id elementum mauris. Vivamus rutrum turpis id nibh ullamcorper varius. Suspendisse dolor justo, sagittis a justo id, hendrerit rutrum velit.
+           Whether you sell food, fashion, gadgets, or offer essential services, HustleHub gives you the space to reach students on campus with ease. Set up your shop, grow your brand, and get discovered by the right customers.
         </p>
         <button className="bg-white text-[#10194A] font-semibold rounded-full px-6 sm:px-8 py-2.5 sm:py-3 text-sm sm:text-base shadow hover:bg-gray-100 transition">
           Become a vendor
@@ -18,9 +18,16 @@ const VendorBanner: React.FC = () => (
       {/* Right Side */}
       <div className="flex-1 flex items-center justify-center w-full max-w-xl">
         <div className="grid grid-cols-2 grid-rows-2 gap-3 sm:gap-4 w-full h-40 sm:h-[320px]">
-          <div className="bg-gray-300 rounded-xl col-span-1 row-span-2 h-full" />
-          <div className="bg-gray-300 rounded-xl col-span-1 row-span-1" />
-          <div className="bg-gray-300 rounded-xl col-span-1 row-span-1" />
+          <img 
+  src="https://pbs.twimg.com/media/FmC619lagAAT9GN.jpg" 
+  alt="Vendor point"
+  className="rounded-xl col-span-1 row-span-2 h-full w-full object-cover"
+/>
+
+          <img src="https://pbs.twimg.com/media/GwC7JrqWwAAg0Hu?format=jpg&name=4096x4096" 
+  alt="Vendor point"
+  className="rounded-xl col-span-1 row-span-2 h-full w-full object-cover"/>
+          
         </div>
       </div>
     </section>
